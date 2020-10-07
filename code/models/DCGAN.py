faf5288abdf0c57a0f4e9e6d481421d6decd4bf2
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-10-07 20:24:35 +1100

    added the implementation for the _build function

--- code/models/DCGAN.py
+++ code/models/DCGAN.py
@@ -5,10 +5,10 @@ class Generator(nn.Module):
 
     def __init__(self, channels, kernel_sizes, strides, paddings, batch_norm, activations):
         """
-        channels: array containing channels for each CONV layer, starting from the z_dim until the output image channels
-        kernel_sizes: array containing kernel sizes for each CONV layer
-        strides: array containing strides for each CONV layer
-        paddings: array containing paddings for each CONV layer
+        channels: array containing channels for each convtranspose layer, starting from the input dimension until the output image channels
+        kernel_sizes: array containing kernel sizes for each convtranspose layer
+        strides: array containing strides for each convtranspose layer
+        paddings: array containing paddings for each convtranspose layer
         batch_norm: True if batch norm is to be used, otherwise, False
         activations: array containing a hidden activation and an output activation
         """
@@ -17,7 +17,23 @@ class Generator(nn.Module):
         self._init_weights()
     
     def _build(self, channels, kernel_sizes, strides, paddings, batch_norm, activations):
-        pass
+        layers = []
+        for i in range(1, channels):
+            # add convtranspose layer
+            layers += [nn.ConvTranspose2d(
+                in_channels=channels[i-1],
+                out_channels=channels[i],
+                kernel_size=kernel_sizes[i],
+                stride=strides[i],
+                padding=paddings[i],
+                bias=False if batch_norm else True
+            )]
+            # add batchnorm
+            if batch_norm and i < len(channels)-1:
+                layers += [nn.BatchNorm2d(channels[i])]
+            # add activation
+            layers += [activations[0] if i < len(channels)-1 else activations[1]]
+        return nn.Sequential(*layers)
 
     def _init_weights(self):
         pass
