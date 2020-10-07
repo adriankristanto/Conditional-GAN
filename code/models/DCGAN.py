bba36f5d8b9ee7e2d5f498386669a50c7ca50db9
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-10-07 20:04:03 +1100

    added the class definition for the generator model

--- code/models/DCGAN.py
+++ code/models/DCGAN.py
@@ -1,2 +1,15 @@
 import torch
 import torch.nn as nn
+
+class Generator(nn.Module):
+
+    def __init__(self, channels, kernel_sizes, strides, paddings, batch_norm, activations):
+        """
+        channels: array containing channels for each CONV layer, starting from the z_dim until the output image channels
+        kernel_sizes: array containing kernel sizes for each CONV layer
+        strides: array containing strides for each CONV layer
+        paddings: array containing paddings for each CONV layer
+        batch_norm: True if batch norm is to be used, otherwise, False
+        activations: array containing a hidden activation and an output activation
+        """
+        super(Generator, self).__init__()
