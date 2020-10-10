9b36b50d739ef4a60db5053efa130ba229bad923
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-10-10 21:35:31 +1100

    load the training dataset

--- code/train_cgan.py
+++ code/train_cgan.py
@@ -8,6 +8,9 @@ from tqdm import tqdm
 import models.DCGAN as DCGAN
 import os
 
+# Hyperparameters
+BATCH_SIZE = 128
+
 if __name__ == "__main__":
     
     # device setup
@@ -28,3 +31,16 @@ if __name__ == "__main__":
     if 'saved_models' not in os.listdir(MAIN_DIR):
         print('creating saved_models directory...', flush=True)
         os.mkdir(MAIN_DIR + 'saved_models')
+    
+    # 1. load the dataset
+    DATA_PATH = MAIN_DIR + 'data'
+    train_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5))
+    ])
+
+    trainset = datasets.MNIST(root=DATA_PATH, download=True, transform=train_transform)
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True)
+
+    print(f"Total training data: {len(trainset)}\n", flush=True)
