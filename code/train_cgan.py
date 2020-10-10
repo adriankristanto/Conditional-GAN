10ac748e582b6a947b2c55b8709d7fcd15a94356
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-10-10 21:26:33 +1100

    setup the project directory

--- code/train_cgan.py
+++ code/train_cgan.py
@@ -6,8 +6,25 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from tqdm import tqdm
 import models.DCGAN as DCGAN
+import os
 
 if __name__ == "__main__":
     
+    # device setup
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Current device: {device}\n", flush=True)
+
+    # directory setup
+    MAIN_DIR = os.path.dirname(os.path.realpath(__file__)) + '/../'
+    if 'data' not in os.listdir(MAIN_DIR):
+        print('creating data directory...', flush=True)
+        os.mkdir(MAIN_DIR + 'data')
+    if 'generated_images' not in os.listdir(MAIN_DIR):
+        print('creating generated_images directory...', flush=True)
+        os.mkdir(MAIN_DIR + 'generated_images')
+    if 'logs' not in os.listdir(MAIN_DIR):
+        print('creating logs directory...', flush=True)
+        os.mkdir(MAIN_DIR + 'logs')
+    if 'saved_models' not in os.listdir(MAIN_DIR):
+        print('creating saved_models directory...', flush=True)
+        os.mkdir(MAIN_DIR + 'saved_models')
