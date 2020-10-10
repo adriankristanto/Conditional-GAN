0f6d50aed9b39698e55d3b31e32a04154a9fd2ea
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-10-10 21:22:30 +1100

    start with the implementation of the training process

---
+++ code/train_cgan.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from tqdm import tqdm
+import models.DCGAN as DCGAN
