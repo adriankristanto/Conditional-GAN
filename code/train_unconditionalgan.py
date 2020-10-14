dc79442db26e18b3f279292530a6488c91052ff4
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-10-14 21:30:22 +1100

    added the hyperparameters

--- code/train_unconditionalgan.py
+++ code/train_unconditionalgan.py
@@ -9,3 +9,27 @@ from tqdm import tqdm
 import models.DCGAN as DCGAN
 import os 
 from utils import CriticLoss, GradientPenaltyLoss, GeneratorLoss
+
+MAIN_DIR = os.path.dirname(os.path.realpath(__file__)) + '/../'
+CONTINUE_TRAIN = False
+CONTINUE_TRAIN_NAME = 'unconditionalgan-model-epoch10.pth'
+EPOCH = 200
+SAVE_INTERVAL = 20
+# for generation
+SAMPLE_INTERVAL = 100
+SAMPLE_SIZE = 32
+# shape of a single image
+INPUT_IMG_SHAPE = (1, 28, 28)
+
+# Hyperparameters
+BATCH_SIZE = 128
+Z_DIM = 100
+GENERATOR_LR = 0.0002
+DISCRIMINATOR_LR = 0.0002
+BETAS = (0.5, 0.999)
+
+# WGAN-GP global variables
+# the recommended ratio used is 5 critic updates to 1 generator update
+CRITIC_ITER = 5
+# the following is the penalty coefficient
+LAMBDA = 10
