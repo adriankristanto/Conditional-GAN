d9bac65ca96f12222e41896e5375f5fdccfe5422
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-10-14 21:32:17 +1100

    add device and directory setup

--- code/train_unconditionalgan.py
+++ code/train_unconditionalgan.py
@@ -33,3 +33,23 @@ BETAS = (0.5, 0.999)
 CRITIC_ITER = 5
 # the following is the penalty coefficient
 LAMBDA = 10
+
+if __name__ == "__main__":
+    
+    # device setup
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Current device: {device}\n", flush=True)
+
+    # directory setup
+    if 'data' not in os.listdir(MAIN_DIR):
+        print('creating data directory...', flush=True)
+        os.mkdir(MAIN_DIR + 'data')
+    if 'generated_images' not in os.listdir(MAIN_DIR):
+        print('creating generated_images directory...', flush=True)
+        os.mkdir(MAIN_DIR + 'generated_images')
+    if 'logs' not in os.listdir(MAIN_DIR):
+        print('creating logs directory...', flush=True)
+        os.mkdir(MAIN_DIR + 'logs')
+    if 'saved_models' not in os.listdir(MAIN_DIR):
+        print('creating saved_models directory...', flush=True)
+        os.mkdir(MAIN_DIR + 'saved_models')
