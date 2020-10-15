2e2e510bd6eb0622bf98b716cf5299e690555b38
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-10-15 11:41:25 +1100

    added device setup

--- code/app_cgan.py
+++ code/app_cgan.py
@@ -5,3 +5,6 @@ from datetime import datetime
 import models.DCGAN as DCGAN
 import os
 from collections import OrderedDict
+
+# device setup
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
